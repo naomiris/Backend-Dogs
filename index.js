d30420375ca4aa350680a44171f5b88d41725e5e
Naomi Del Castillo <89600868+naomiris@users.noreply.github.com>
2022-02-25 13:42:23 -0300

    Update index.js

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ const { conn } = require('./src/db.js');
 
 // Syncing all the models at once.
 conn.sync({ force: false }).then(() => {
-  server.listen(3005, () => {
+  server.listen(process.env.PORT || 3005, () => {
     console.log('%s listening at 3005'); // eslint-disable-line no-console
   });
 });
